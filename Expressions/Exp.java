7c3d9e60d1bed6da8e73f4bec1b191752847d4fb
vicinity123 <michaelt.abayneh@gmail.com>
2022-03-19 11:10:19 -0400

    Resolved decimal exponent problem and division by zero error

--- Expressions/Exp.java
+++ Expressions/Exp.java
@@ -23,6 +23,7 @@ public class Exp {
         for (int i = 0; i < expr.length; i++) {
             if (expr[i] == 37 || expr[i] == 42 || expr[i] == 43 || expr[i] == 45 || expr[i] == 47 || expr[i] == 94) {
                 operator = expr[i];
+                continue;
             }
             while (i + numLength < expr.length && ((expr[i + numLength] > 47 && expr[i + numLength] < 58) || (expr[i + numLength] == 46))) {
                 numLength++;
@@ -32,15 +33,26 @@ public class Exp {
             } else {
                 num2 = String.valueOf(Arrays.copyOfRange(expr, i, i + numLength));
             }
+            i += numLength - 1;
+            // Reset numLength to 0
+            numLength = 0;
         }
-        if ((num1.equals("") != true) && (num2.equals("") != true) && (operator != '\u0000')) {
-            result = calculate(Double.parseDouble(num1), operator, Double.parseDouble(num2));
-            System.out.println(result);
+        if (num1.equals("") != true && num2.equals("") != true && operator != '\u0000') {
+            try {
+                result = calculate(Double.parseDouble(num1), operator, Double.parseDouble(num2));
+                System.out.println(result);
+            } catch (IllegalArgumentException e) {
+                System.out.println("Invalid divisor input. (Division by zero error)");
+            }
         } else {
             System.out.println("Invalid input given...");
         }
     }
     public double calculate(double num1, char operator, double num2) {
+        // EDGE CASES
+        if(operator == '/' && num2 == 0) {
+            throw new IllegalArgumentException();
+        }
         if (operator == '+') { // ASCII: 43
             return num1 + num2;
         } else if (operator == '-') { // ASCII: 45
